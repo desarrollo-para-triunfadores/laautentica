9b694961b799aed2b09cb8905e69c664f5b86da7
Hacho Kuszniruk <hacho.kuszniruk@gmail.com>
2016-01-06 23:04:54 -0300

    arreglo en la vista para dispositivos moviles

--- resources/views/admin/partes/estilos.blade.php
+++ resources/views/admin/partes/estilos.blade.php
@@ -1,4 +1,7 @@
-	<link type="text/css" rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,300,700">
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link type="text/css" rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,300,700">
     <link type="text/css" rel="stylesheet" href="http://fonts.googleapis.com/css?family=Oswald:400,700,300">
     <link type="text/css" rel="stylesheet" href="{{ asset('plantillas/KAdmin-Dark/styles/jquery-ui-1.10.4.custom.min.css') }}">
     <link type="text/css" rel="stylesheet" href="{{ asset('plantillas/KAdmin-Dark/styles/font-awesome.css') }}  ">
@@ -11,19 +14,8 @@
     <link type="text/css" rel="stylesheet" href="{{ asset('plantillas/KAdmin-Dark/styles/zabuto_calendar.min.css') }}">
     <link type="text/css" rel="stylesheet" href="{{ asset('plantillas/KAdmin-Dark/styles/pace.css') }} ">
     <link type="text/css" rel="stylesheet" href="{{ asset('plantillas/KAdmin-Dark/styles/jquery.news-ticker.css') }}">
-
-
-
-
     <link type="text/css" rel="stylesheet" href="{{ asset('plantillas/bootstrap-select/css/bootstrap-select.min.css') }}">
     <link type="text/css" rel="stylesheet" href="{{ asset('plantillas/bootstrap-select/css/bootstrap-select.css') }}">
-    
-
-
     <link href="{{ asset('plantillas/parsley/src/parsley.css') }}" rel='stylesheet' type='text/css'>
-
     <link href="{{ asset('plantillas/DataTables/media/css/jquery.dataTables.css') }}" rel='stylesheet' type='text/css'>
     <link href="{{ asset('plantillas/DataTables/extensions/Responsive/css/dataTables.responsive.css') }}" rel='stylesheet' type='text/css'>
-
-
-        
